20b7cb7c01aa18c14b3be07663c0536f701e5df8
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2022-07-21 20:14:13 +0100

    tidy up

--- opensocialmediaapp/api/serializers.py
+++ opensocialmediaapp/api/serializers.py
@@ -1,17 +1,4 @@
-# from django.contrib.auth.models import User, Group
-# from rest_framework import serializers
 
-
-# class UserSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ['url', 'username', 'email', 'groups']
-
-
-# class GroupSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = Group
-#         fields = ['url', 'name']
 
 from rest_framework import serializers
 import uuid
@@ -44,7 +31,3 @@ class UserSerializer(serializers.ModelSerializer):
         read_only_fields = [
             'id'
         ]
-
-
-# class LoginSerializer(serializers.ModelSerializer):
-  # id:
--- opensocialmediaapp/urls.py
+++ opensocialmediaapp/urls.py
@@ -1,37 +1,4 @@
-# """opensocialmediaapp URL Configuration
 
-# The `urlpatterns` list routes URLs to views. For more information please see:
-#     https://docs.djangoproject.com/en/4.0/topics/http/urls/
-# Examples:
-# Function views
-#     1. Add an import:  from my_app import views
-#     2. Add a URL to urlpatterns:  path('', views.home, name='home')
-# Class-based views
-#     1. Add an import:  from other_app.views import Home
-#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-# Including another URLconf
-#     1. Import the include() function: from django.urls import include, path
-#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-# """
-# # from django.contrib import admin
-# # from django.urls import path
-
-# # urlpatterns = [
-# #     path('admin/', admin.site.urls),
-# # ]
-
-# from django.urls import include, path
-# from rest_framework import routers
-# from opensocialmediaapp.api import views
-# router = routers.DefaultRouter()
-# router.register(r'users', views.UserViewSet)
-# router.register(r'groups', views.GroupViewSet)
-# # Setup automatic URL routing
-# # Additionally, we include login URLs for the browsable API.
-# urlpatterns = [
-#     path('', include(router.urls)),
-#     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-# ]
 """opensocialmediaapp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -64,12 +31,9 @@ from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 from .api.models import User, UserLoginTokens
 from django.contrib.auth.hashers import make_password, check_password
 
-# from django.conf.urls import handler403, handler404, handler500, url
-
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
-# router.register(r'login', views.LoginViewSet)
 
 
 @api_view(['GET'])
@@ -97,7 +61,6 @@ def get_tokens_for_user(request):
         access_token=access, refresh_token=refresh, user_id=user_id)
 
     saved_token.save()
-    # return Response({'Success': 'New user created'})
 
     return Response({
         'access': str(access),
@@ -124,9 +87,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    # path('user/login_dan', views.UserViewSet.login_user_to_app_dan, name='enterprise')
-    # path('testing123', views.TestUser.login_user_to_app_daniel)
     path('token/', get_tokens_for_user, name='token_obtain_pair'),
     path('refresh_token/', refresh_token, name='refresh_token')
-    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
